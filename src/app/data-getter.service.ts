c43e83efd6aa6134ffda978b0c179efbacade617
Arnaud C <djcase001@gmail.com>
2018-08-01 18:03:38 -0400

    cleans unnecessary methods

--- src/app/data-getter.service.ts
+++ src/app/data-getter.service.ts
@@ -10,14 +10,6 @@ import { catchError, map, tap } from 'rxjs/operators';
 export class DataGetterService {
   constructor(private _http: Http) {}
 
-  log(msg, result): void {
-    console.log(msg, result);
-  }
-
-  handleError(text, arr): void {
-    console.log(text, arr);
-  }
-
   public getData(by): Observable<any> {
     let url = `https://www.alphavantage.co/query?function=${by}&symbol=MSFT&apikey=demo`;
     return this._http.get(url).pipe(result => result);
